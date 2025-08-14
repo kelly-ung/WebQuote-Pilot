2e4b31d6b109befb8c0f0ceef10e972065cc3346
Kelly Ung <kellyung@myyahoo.com>
2025-08-14 15:05:43 -0700

    add scroll to quote function

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const getGeminiResponse= async (rawText: string, apiKey: string) => {
     const max = 20000; 
     const text = rawText.length > max ? rawText.slice(0, max) : rawText; // Truncate text if it exceeds the maximum length
-    const prompt = `Generate quotes from the following text separated by bullets:\n\n${text}`;
+    const prompt = `From the following text, extract the most significant phrases or sentences. Present each one exactly as it appears in the original text, without additional quotation marks, separated by bullet points:\n\n${text}`;
 
     // Make a POST request to the Gemini API
     try {
@@ -85,8 +85,23 @@ function App() {
     chrome.runtime.openOptionsPage(); 
   };
 
+  // Function to handle scrolling to a quote
+  const handleScrollToQuote = async (query: string) => {
+    const trimQuery = query.trim(); // Remove any whitespace from ends
+
+    try {
+      let [tab] = await chrome.tabs.query({ active: true, currentWindow: true }); // Get the active tab in the current window
+      const response = await chrome.tabs.sendMessage(tab.id!, { type: 'scrollToQuote', query: trimQuery }); // Send a message to the content script to scroll to quote
+      console.log(response) // Log Success or Error
+    }
+    catch (error) {
+      console.log("Error:", error)
+    }
+  }
+
   return (
     <>
+      { /* Settings button */}
       <button onClick={handleSettings} className="absolute top-4 right-4 px-4 py-2 cursor-pointer transition-transform hover:scale-105">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
@@ -97,6 +112,7 @@ function App() {
       <div>
         <h1 className="text-3xl font-bold mt-6 mb-6">AI Quote Generator</h1>
 
+        { /* Show when loading  */}
         {loading && (
             <div className="flex flex-col items-center justify-center">
               <div className="mb-4 w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
@@ -104,12 +120,16 @@ function App() {
             </div>
         )}
 
+        { /* Show any errors */}
         {error && <div className="text-red-400">{error}</div> }
 
+        { /* Display generated quotes */}
         {result && (
           <ul className="list-disc ml-6">
             {result.map((item, index) => (
-              <li key={index} className="mb-2">{item}</li> 
+              <button onClick={() => handleScrollToQuote(item)} className="hover:bg-[#206eaa] cursor-pointer">
+                <li key={index} className="mb-2">{item}</li> 
+              </button>
             ))}
           </ul>
         )}
