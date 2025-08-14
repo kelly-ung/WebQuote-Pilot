8f0ccedfe69554e3a2c84f356ff7312d381f1e73
Kelly Ung <kellyung@myyahoo.com>
2025-08-14 15:06:42 -0700

    add scrollToQuote to listener

--- public/content.js
+++ public/content.js
@@ -13,7 +13,18 @@ function extractText() {
 // Listener to run extractText function when user clicks the button in the extension popup
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
     if (req.type === "extractText") {
+        console.log("extract text ran")
         const text = extractText();
         sendResponse(text);
+    }
+    else if (req.type === "scrollToQuote") {
+        const query = req.query
+        const found = window.find(query, false, false, true, false, false, false); // Scrolls to text found on the web page
+        if (!found) {
+            sendResponse('Error');
+        }
+        else {
+            sendResponse('Success');
+        }
     }
 });
